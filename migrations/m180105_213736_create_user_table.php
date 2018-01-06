723c2989de4c5e9a6df078ee6228352503ec4d9f
man780 <man780@mail.ru>
2018-01-06 08:17:10 +0500

    user migrate adminLTE

---
+++ migrations/m180105_213736_create_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user`.
+ */
+class m180105_213736_create_user_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'email' => $this->string()->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'status' => $this->integer()->notNull(),
+            'role' => $this->integer(),
+            'auth_key' => $this->string(32)->notNull(),
+            'secret_key' => $this->string(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+}
